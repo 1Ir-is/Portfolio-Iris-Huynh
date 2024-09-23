03cc25f86679919f743df0cf54c2fdfdb33aa2c0
1Ir-is <mhuynk1005@gmail.com>
2024-09-23 13:58:43 +0700

    add file

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// components
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:px-12 text-white">
+      <div className="container mx-auto">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Iris Huynh<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* Desktop Nav */}
+
+        <Nav />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
