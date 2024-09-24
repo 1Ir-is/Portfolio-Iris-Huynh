b27b20225b159f0b199fc9d6a9c3a55d896885ad
1Ir-is <mhuynk1005@gmail.com>
2024-09-24 13:36:59 +0700

    feat: init the work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,4 +1,24 @@
-import React from "react";
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+
+import Link from "next/link";
 
 const Work = () => {
   return <div>Work</div>;
